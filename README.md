7246d9827a5519bfcc5f22bb87e21616725a90df
Szymon Nogieć <szymon.nogiec@gmail.com>
2021-12-09 19:56:37 +0100

    simple http server

---
+++ README.md
@@ -0,0 +1,7 @@
+http_server
+-----------
+
+Simple http server implementation in rust.
+
+## Running
+Build with `cargo build`and run with `cargo run`
---
+++ src/bin/main.rs
@@ -0,0 +1,45 @@
+extern crate http_server;
+
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+use http_server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established");
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+    println!("Shutting down.")
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
